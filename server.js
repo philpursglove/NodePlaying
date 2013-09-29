12552f3a82cc65ac8d785498e4d1c378b451a79d
Phil Pursglove <phil@philippursglove.com>
2013-09-29 08:04:41 +0100

    CHanged format to html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<html><head><title>Node.js for fun and profit</title></head><body>Hello World</body></html>');
 }).listen(port);
